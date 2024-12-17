0cb030e78c4f640c790f381df3ebd75a657288fb
Aquila0911 <siddharth.ghosh9@gmail.com>
2024-12-17 17:38:07 +0530

    cors setup

--- server/index.js
+++ server/index.js
@@ -4,11 +4,15 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const connectDB = require("./src/config/db.js");
-const pointsRoute = require("./src/routes/updatePoints.js")
-
+const pointsRoute = require("./src/routes/updatePoints.js");
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Add methods you need
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
@@ -20,10 +24,10 @@ app.get("/api/test", (req, res) => {
 });
 
 // Routes
-app.use(pointsRoute)
+app.use(pointsRoute);
 
 // Start Sever
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}...`);
+  console.log(`Server running on port ${port}...`);
 });
